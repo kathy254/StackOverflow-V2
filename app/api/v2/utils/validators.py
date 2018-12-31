6da9eac1a0c7c68b8347be2fb91e25667799c967
Kathy <catherine.omondi2@gmail.com>
2018-12-31 07:30:45 +0300

    changed validations

--- app/api/v2/utils/validators.py
+++ app/api/v2/utils/validators.py
@@ -1,13 +1,13 @@
 import re
 
 
-class Validations:
+class Verify:
     def is_empty(self, items):
         for item in items:
-            if bool(item) is True:
+            if bool(item) is False:
                 return True
         return False
-
+               
 
     def is_whitespace(self, items):
         for item in items:
@@ -16,47 +16,45 @@ class Validations:
         return False
 
 
-    def payload(self, items, keys, length):
-        keys = "email_address" or "username" or "password"
+    def payload(self, items, length, keys):
         items = items.keys()
         if len(items) == length:
             for item in items:
                 if item not in keys:
                     return False
-            return True
+                return True
 
 
     def is_signup_payload(self, items):
-        res = self.payload(items, 3, ["email_address", "username", "password"])
-        return res
-
-
-    def is_login_payload(self, items):
-        res = self.payload(items, 2, ["username", "password"])
+        res = self.payload(items, 3, ['email_address', 'username', 'password'])
         return res
 
 
     @staticmethod
     def is_valid_email(email_address):
-        if re.match(r"(^[a-zA-Z0-9_+.-]+@[a-zA-Z0-9-.]+\.[a-zA-Z0-9-.]+$)", email_address) is None:
-            res = False
-        else:
+        if re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email_address) is None:
             res = True
+        else:
+            res = False
         return res
 
-
+    
     @staticmethod
     def is_valid_password(password):
-        if (len(password)<6 or len(password)>12) is True:
-            res = False
-        else:
+        if (len(password)<6) is True:
             res = True
+        else:
+            res = False
+        return res
+
+
+    def is_login_payload(self, items):
+        res = self.payload(items, 2, ['username', 'password'])
         return res
 
 
     @staticmethod
-    def list_iterator(_list):
-        for i in _list:
+    def list_iterator(list):
+        for i in list:
             if i is None or not i:
                 return False
-                
